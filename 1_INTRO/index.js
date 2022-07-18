a182435069643cc83959138e7190f284ace5aa6f
GeorgeSalu <george.salu10@gmail.com>
2022-07-18 17:22:17 -0300

    primeiro programa com ts

---
+++ 1_INTRO/index.js
@@ -0,0 +1,7 @@
+var firstName = "Mateus";
+var anotherName = 1;
+var x = 1;
+function greeting(name) {
+    console.log("Ola " + name);
+}
+greeting(firstName);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,9 @@
+const firstName = "Mateus"
+const anotherName = 1
+const x = 1
+
+function greeting(name: string) {
+  console.log("Ola "+name)
+}
+
+greeting(firstName)
