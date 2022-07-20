80bbecb3d2c3b49110fde6b7e6daa40258fa4f9b
GeorgeSalu <george.salu10@gmail.com>
2022-07-20 15:32:38 -0300

    arrays

---
+++ 3_AVANCANDO_EM_TIPOS/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// 1 - array
+let numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers);
+const nomes = ["matheus", "george"];
+console.log(nomes);
---
+++ 3_AVANCANDO_EM_TIPOS/index.ts
@@ -0,0 +1,7 @@
+// 1 - array
+let numbers: number[] = [1,2,3]
+numbers.push(5)
+console.log(numbers)
+
+const nomes: string[] = ["matheus", "george"]
+console.log(nomes)
