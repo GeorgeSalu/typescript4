85b7e606f0ae11371dc3228ee0b0b8e51345e537
GeorgeSalu <george.salu10@gmail.com>
2022-07-21 14:37:34 -0300

    checando se existe valor

--- 4_NARROWING/index.js
+++ 4_NARROWING/index.js
@@ -14,3 +14,22 @@ function sum(a, b) {
 sum("4", "59");
 sum(12, 42);
 sum("5", 42);
+// 2- checando se valor existe 
+function operations(arr, operation) {
+    if (operation) {
+        if (operation === "sum") {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        }
+        else if (operation === "multiply") {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        }
+    }
+    else {
+        console.log("por favor, defina uma operacao");
+    }
+}
+operations([1, 2, 3]);
+operations([1, 2, 3], "sum");
+operations([1, 2, 3], "multiply");
