1d37d393e808575544786b5a47a0497b87393a3e
GeorgeSalu <george.salu10@gmail.com>
2022-07-21 16:30:30 -0300

    callback sem argumentos

--- 5_FUNCOES/index.js
+++ 5_FUNCOES/index.js
@@ -4,3 +4,14 @@ function withoutReturn() {
     console.log("esta funcao nao tem retorno");
 }
 withoutReturn();
+// 2 - callback com argumento
+function greeting(name) {
+    return `Ola ${name}`;
+}
+function preGreeting(f, userName) {
+    console.log("preparando a funcao");
+    const greet = f(userName);
+    console.log(greet);
+}
+preGreeting(greeting, "george");
+preGreeting(greeting, "joao");
--- 5_FUNCOES/index.ts
+++ 5_FUNCOES/index.ts
@@ -4,3 +4,19 @@ function withoutReturn(): void {
 }
 
 withoutReturn()
+
+// 2 - callback com argumento
+function greeting(name: string): string {
+  return `Ola ${name}`
+}
+
+function preGreeting(f: (name: string) => string, userName: string) {
+  console.log("preparando a funcao")
+
+  const greet = f(userName)
+
+  console.log(greet)
+}
+
+preGreeting(greeting, "george")
+preGreeting(greeting, "joao")
