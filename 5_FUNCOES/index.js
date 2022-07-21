d5845a2b4bce33dbe3eabc7fffa3fea131a2581f
GeorgeSalu <george.salu10@gmail.com>
2022-07-21 16:24:10 -0300

    funcoes sem retorno

---
+++ 5_FUNCOES/index.js
@@ -0,0 +1,6 @@
+"use strict";
+// 1 - void
+function withoutReturn() {
+    console.log("esta funcao nao tem retorno");
+}
+withoutReturn();
---
+++ 5_FUNCOES/index.ts
@@ -0,0 +1,6 @@
+// 1 - void
+function withoutReturn(): void {
+  console.log("esta funcao nao tem retorno")
+}
+
+withoutReturn()
