d6be2fa922f6161fc0f5e466d9bb85beb884c871
GeorgeSalu <george.salu10@gmail.com>
2022-07-21 14:52:52 -0300

    operador instanceof

--- 4_NARROWING/index.ts
+++ 4_NARROWING/index.ts
@@ -31,3 +31,35 @@ function operations(arr: number[], operation?: string | undefined) {
 operations([1,2,3])
 operations([1,2,3], "sum")
 operations([1,2,3], "multiply")
+
+// 3 - instanceOd
+class User {
+  name
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+class SuperUser extends User {
+  constructor(name: string) {
+    super(name)
+  }
+}
+
+const jhon = new User("Jhon")
+const paul = new SuperUser("Paul")
+
+console.log(jhon)
+console.log(paul)
+
+function userGreeting(user: object) {
+  if(user instanceof SuperUser) {
+    console.log(`Ola ${user.name}. deseja ver os dados do sistema ?`)
+  } else if (user instanceof User) {
+    console.log(`Ola ${user.name}`)
+  }
+}
+
+userGreeting(jhon)
+userGreeting(paul)
