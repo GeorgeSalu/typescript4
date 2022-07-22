881126fd6cc733f5ca87f601e48cc4bd0ec96e83
GeorgeSalu <george.salu10@gmail.com>
2022-07-22 20:38:20 -0300

    revisando generics

---
+++ 7_CRIACAO_DE_TIPOS/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - generics
+function showData(arg) {
+    return `O dado é : ${arg}`;
+}
+console.log(showData(5));
+console.log(showData("testando generics"));
+console.log(showData(true));
