675032bb1c6607fbbe4b1e23a4113001e99122cc
GeorgeSalu <george.salu10@gmail.com>
2022-07-25 13:29:46 -0300

    constructor

--- 8_CLASSES/index.js
+++ 8_CLASSES/index.js
@@ -6,3 +6,12 @@ const matheus = new User();
 console.log(matheus);
 matheus.name = "mateus";
 console.log(matheus);
+// 2 - constructor
+class NewUser {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+const joao = new NewUser("joao", 22);
+console.log(joao);
--- 8_CLASSES/index.ts
+++ 8_CLASSES/index.ts
@@ -8,3 +8,17 @@ const matheus = new User()
 console.log(matheus)
 matheus.name="mateus"
 console.log(matheus)
+
+// 2 - constructor
+class NewUser {
+  name
+  age
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+}
+
+const joao = new NewUser("joao", 22)
+console.log(joao)
