7a4581c49de6a3645b39574ba343249a27fb65cb
GeorgeSalu <george.salu10@gmail.com>
2022-07-25 09:49:23 -0300

    reduzindo tipos aceitos com generics

--- 7_CRIACAO_DE_TIPOS/index.js
+++ 7_CRIACAO_DE_TIPOS/index.js
@@ -6,3 +6,12 @@ function showData(arg) {
 console.log(showData(5));
 console.log(showData("testando generics"));
 console.log(showData(true));
+// 2 - constraint em generci
+function showProductName(obj) {
+    return `O nome do produto é : ${obj.name}`;
+}
+const myObj = { name: "porta", cor: "branca" };
+const otherProduct = { name: "carro", wheels: 4, engine: 1.0 };
+const thirdObj = { price: 19.9, category: "roupa" };
+console.log(showProductName(myObj));
+console.log(showProductName(otherProduct));
--- 7_CRIACAO_DE_TIPOS/index.ts
+++ 7_CRIACAO_DE_TIPOS/index.ts
@@ -6,3 +6,15 @@ function showData<T>(arg: T): string {
 console.log(showData(5))
 console.log(showData("testando generics")) 
 console.log(showData(true))
+
+// 2 - constraint em generci
+function showProductName<T extends {name: string}>(obj: T) {
+  return `O nome do produto é : ${obj.name}`
+}
+
+const myObj = {name: "porta", cor: "branca"}
+const otherProduct = {name: "carro", wheels: 4, engine: 1.0}
+const thirdObj = {price: 19.9, category: "roupa"}
+
+console.log(showProductName(myObj))
+console.log(showProductName(otherProduct))
