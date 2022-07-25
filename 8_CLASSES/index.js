e2ea818ba9363535cb1d397136f7a9a6b0588378
GeorgeSalu <george.salu10@gmail.com>
2022-07-25 11:45:06 -0300

    campos em classes

---
+++ 8_CLASSES/index.js
@@ -0,0 +1,8 @@
+"use strict";
+// 1 - campos em classes
+class User {
+}
+const matheus = new User();
+console.log(matheus);
+matheus.name = "mateus";
+console.log(matheus);
---
+++ 8_CLASSES/index.ts
@@ -0,0 +1,10 @@
+// 1 - campos em classes
+class User {
+  name!: string
+  age!: number
+}
+
+const matheus = new User()
+console.log(matheus)
+matheus.name="mateus"
+console.log(matheus)
