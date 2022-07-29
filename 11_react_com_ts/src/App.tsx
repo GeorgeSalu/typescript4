24aa4bdc52e8aa410e581a66b84777237455cfb8
GeorgeSalu <george.salu10@gmail.com>
2022-07-29 13:55:16 -0300

    variaveis com react

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css';
 
 function App() {
+  // 1 - variaveis
+  const name: string = "george"
+  const age: number = 30
+  const isWorking: boolean = false
+
   return (
     <div className="App">
       <h1>Typescript com react</h1>
+      <h2>Nome: {name}</h2>
+      <h2>Idade: {age}</h2>
+      {isWorking && (
+        <div>
+          <p>Esta trabalhando</p>
+        </div>
+      )}
     </div>
   );
 }
