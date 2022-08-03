1f512520932e928a6197ed6b33959d3510e4dfff
GeorgeSalu <george.salu10@gmail.com>
2022-08-03 10:04:34 -0300

    css do formulario

--- 12_todo_react/src/components/TaskForm.tsx
+++ 12_todo_react/src/components/TaskForm.tsx
@@ -1,19 +1,23 @@
+import styles from './TaskForm.module.css'
+
 interface Props {
   btnText: string
 }
 
 const TaskForm = ({btnText}: Props) => {
-  return <form>
-    <div>
-      <label htmlFor="title">Titulo:</label>
-      <input type="text" name="title" placeholder="titulo da tarefa" />
-    </div>
-    <div>
-      <label htmlFor="difficulty">Dificuldade:</label>
-      <input type="text" name="difficulty" placeholder="dificulade da tarefa" />
-    </div>
-    <input type="submit" value={btnText} />
-  </form>
+  return (
+    <form className={styles.form}>
+      <div className={styles.input_container}>
+        <label htmlFor="title">Titulo:</label>
+        <input type="text" name="title" placeholder="titulo da tarefa" />
+      </div>
+      <div className={styles.input_container}>
+        <label htmlFor="difficulty">Dificuldade:</label>
+        <input type="text" name="difficulty" placeholder="dificulade da tarefa" />
+      </div>
+      <input type="submit" value={btnText} />
+    </form>
+  )
 }
 
 export default TaskForm
