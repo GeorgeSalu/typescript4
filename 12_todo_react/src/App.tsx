c846cf4bf2ce0eb6a6f642c2cee361c100fbabca
GeorgeSalu <george.salu10@gmail.com>
2022-08-04 15:35:55 -0300

    adicionando tarefas

--- 12_todo_react/src/App.tsx
+++ 12_todo_react/src/App.tsx
@@ -4,15 +4,20 @@ import Header from './components/Header';
 import styles from "./App.module.css"
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
+import { useState } from 'react';
+import { ITask } from './interfaces/Task';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
       <main className={styles.main}>
         <div>
           <h2>o que vc vai fazer</h2>
-          <TaskForm btnText='criar tarefa' />
+          <TaskForm btnText='criar tarefa' taskList={taskList} setTaskList={setTaskList} />
         </div>
         <div>
           <h2>Suas tarefas:</h2>
