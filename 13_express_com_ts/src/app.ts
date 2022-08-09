73adf4fbd39ff29d3714c25c1a13b3505e5f95c5
GeorgeSalu <george.salu10@gmail.com>
2022-08-09 11:08:41 -0300

    inicnando o express

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -1,2 +1,15 @@
 // 1 - iniciando projeto
-console.log("express + ts !!")
+//console.log("express + ts !!")
+
+// 2 - init express
+import express from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+  return res.send("hello express")
+})
+
+app.listen(3000, () => {
+  console.log("aplicação de ts + express funcionando")
+})
