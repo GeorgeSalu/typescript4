ab1f1de9fc3d26639728d89bc4232a1dd810020e
GeorgeSalu <george.salu10@gmail.com>
2022-08-09 11:18:00 -0300

    rota com post

--- 13_express_com_ts/src/app.ts
+++ 13_express_com_ts/src/app.ts
@@ -6,8 +6,18 @@ import express from 'express'
 
 const app = express()
 
+// 3 - rota com post
+app.use(express.json())
+
 app.get("/", (req, res) => {
   return res.send("hello express")
+})
+
+// 3 - rota com post
+app.post("/api/product", (req, res) => {
+  console.log(req.body);
+
+  return res.send("produto adicionado")
 })
 
 app.listen(3000, () => {
